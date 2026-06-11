1564abb8dce8749d5e8306a41390d73b8c2ece1a
Jack Diamond <jack.diamond22@yahoo.com>
2026-06-11 19:43:45 -0400

    Problem 1 solved!

--- 1-25/problem_001.py
+++ 1-25/problem_001.py
@@ -3,3 +3,19 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
+def is_multiple(limit:int, num_1:int, num_2:int):
+    """
+    Function takes 3 integers, one number to find sum below (limit) and two numbers to find multiples of below the limit
+    """
+    mulitples = []
+    for i in range(limit):
+        if i % num_1 == 0 or i % num_2 == 0:
+            mulitples.append(i)
+
+    total = 0
+    for num in mulitples:
+        total += num 
+    return total 
+
+
+print(is_multiple(1000,3,5))
